a6d0e69553f351102437e0928ccbf2d5db9f544d
Jason Kim <kimjeonghyon@gmail.com>
2020-11-24 09:37:36 +0900

    Update web_monitor.py

--- MachineLearningTips/web_monitor.py
+++ MachineLearningTips/web_monitor.py
@@ -14,19 +14,19 @@ import numpy as np
 fromDt = '20200101'
 #toDt = '20200101'
 #yyyymm = '202001'
-web_id = 'bitest15'
-web_pass = 'P@ssword01'
+web_id = '11111111'
+web_pass = '22222222'
 
 browser = webdriver.Chrome(r"/Users/jeonghyonkim/Documents/Dev/chromedriver")
 
-browser.get('https://dt.tmoney.co.kr/')
+browser.get('https://55555555555')
 time.sleep(3)
 browser.find_element_by_name('id').send_keys(web_id)
 browser.find_element_by_name('pd').send_keys(web_pass)
 browser.find_element_by_xpath('//*[@id="login"]/div[2]/div/div[2]/a/img').click()
 
 
-browser.get('https://dt.tmoney.co.kr/com/main.do?PID=BDPM015001001')
+browser.get('https://7676867o?PID=8787')
 time.sleep(3)
 
 
